93798878d5cae609667693b0e9b290300e8bc24c
Ivor King <ivorking@gmail.com>
2018-08-21 15:38:07 +0100

    first commit for phone practice page

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Run checks
+console.log("javascript connected");
+
+$(document).ready(function() {
+    if (jQuery) {  
+      console.log("jquery working");
+    } else {
+      console.log("jquery not working");
+    }
+});
+
