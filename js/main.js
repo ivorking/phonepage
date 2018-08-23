aaa790b2fb1fb85a81f7c7636b85438d96b245e3
Ivor King <ivorking@gmail.com>
2018-08-23 12:19:33 +0100

    update

--- js/main.js
+++ js/main.js
@@ -10,8 +10,9 @@ if (jQuery) {
 
 // set global variables
 var currentChoice = {
-  colour: "Grey",
-  size: "64GB",
+  colour: "grey",
+  name: "Space Grey",
+  memory: "64GB",
   upfront: "",
   monthly: ""
 }
@@ -20,50 +21,69 @@ var currentChoice = {
 
 const setColour = (setNow) => {
   currentChoice.colour = setNow;
+  $('#choicetext').text(currentChoice.name);
   let setString = ".colour" + setNow + "border";
   $(setString).css("border-color", "#3C676E");
-  if (setNow != "Space Grey") {
+  if (setNow != "grey") {
     $('.colourgreyborder').css("border-color", "white");
   }
-  if (setNow != "Gold") {
+  if (setNow != "gold") {
     $('.colourgoldborder').css("border-color", "white");
   }
-  if (setNow != "Silver") {
+  if (setNow != "silver") {
     $('.coloursilverborder').css("border-color", "white");
   }
 }
 
 const updatePrices = () => {
   for (let i = 0; i < 6; i++) {
-    if ((currentChoice.colour == dataStore.deviceSummary[i].colourName) && (currentChoice.size == dataStore.deviceSummary[i].memory)) {
+    if ((currentChoice.name == dataStore.deviceSummary[i].colourName) && (currentChoice.memory == dataStore.deviceSummary[i].memory)) {
       currentChoice.upfront = dataStore.deviceSummary[i].priceInfo.hardwarePrice.oneOffDiscountPrice.gross;
       currentChoice.monthly = dataStore.deviceSummary[i].priceInfo.bundlePrice.monthlyPrice.gross;
-      $('#upfrontcost').text(currentChoice.upfront);
-      $('#monthlycost').text(currentChoice.monthly);
+      if (currentChoice.upfront) {
+        $('#upfrontcost').text("£" + currentChoice.upfront);
+      } else {
+        $('#upfrontcost').text("£0");
+      }
+      $('#monthlycost').text("£" + currentChoice.monthly);
       return;
     }
   }
 }
 
-// check to see if any buttons have been clicked and update accordingly
+// check to see if any buttons have been clicked and update DOM accordingly
 
 $('.colourgold').on('click', function () {
+  currentChoice.name = "Gold";
   $('#currentimage').attr("src", "images/Apple_iPhone_8_Gold-full-product-front.png");
-  $('#choicetext').text("Gold");
-  setColour("Gold");
-  updatePrices()
+  setColour("gold");
+  updatePrices();
 });
 
 $('.coloursilver').on('click', function () {
+  currentChoice.name = "Silver";
   $('#currentimage').attr("src", "images/Apple_iPhone_8_Silver_WS2-full-product-front.png");
-  $('#choicetext').text("Silver");
-  setColour("Silver");
+  setColour("silver");
   updatePrices();
 });
 
 $('.colourgrey').on('click', function () {
+  currentChoice.name = "Space Grey";
   $('#currentimage').attr("src", "images/Apple_iPhone_8_Space_Grey_WS2-full-product-front.png");
-  $('#choicetext').text("Space Grey");
-  setColour("Space Grey");
+  setColour("grey");
+  updatePrices();
+});
+
+$('.capacityimage64').on('click', function () {
+  currentChoice.memory = "64GB";
+  $('.capacityimage64').css("border-color", "#3C676E");
+  $('.capacityimage256').css("border-color", "white");
+  updatePrices();
+});
+
+$('.capacityimage256').on('click', function () {
+  currentChoice.memory = "256GB";
+  $('.capacityimage256').css("border-color", "#3C676E");
+  $('.capacityimage64').css("border-color", "white");
   updatePrices();
 });
